4d0fa7bf148851858133a44372150220537f1ee5
Rimberse <kerimpltm@gmail.com>
2023-11-28 22:31:46 +0100

    Define interface for exam participants

--- frontend/types/exam.ts
+++ frontend/types/exam.ts
@@ -6,3 +6,10 @@ export interface Exam {
     endDate: Date;
     average: number;
 }
+
+export interface Participant {
+    code: string;
+    name: string;
+    size: number;
+    description?: string;
+}
