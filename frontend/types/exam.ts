bd9e4eafc30c1da117127a6e33c7d9bf404d999d
Rimberse <kerimpltm@gmail.com>
2023-11-28 22:36:20 +0100

    Modify Exam type, add list of participants, make duration optional:

--- frontend/types/exam.ts
+++ frontend/types/exam.ts
@@ -1,12 +1,15 @@
+// Exam type
 export interface Exam {
     id: string;
     name: string;
-    duration: number;
+    duration?: number;
     startDate: Date;
     endDate: Date;
-    average: number;
+    average?: number;
+    participants: Array<Participant>;
 }
 
+// Student group having an access to exam
 export interface Participant {
     code: string;
     name: string;
