2928c014807545325a3c3dcc45511b54bab0e7da
za1nzafar <zainzafar@outlook.fr>
2023-11-30 17:26:45 +0100

    Added BackendApplication

---
+++ backend/src/main/java/net/examng/backend/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package net.examng.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
