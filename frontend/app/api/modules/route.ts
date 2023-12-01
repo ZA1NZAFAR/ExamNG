eb485c099219dae0b2cb4b5f2052e492fd22a051
afkbeto <qv.nguyen00@gmail.com>
2023-12-01 16:00:10 +0100

    remove API and mock files (to be added later)

--- frontend/app/api/modules/route.ts
+++
@@ -1,6 +0,0 @@
-import { mockModules } from '@/mockData/question';
-
-export async function GET() {
-	const response = mockModules;
-	return Response.json(response);
-}
--- frontend/mockData/user.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@/types/user";
-
-export const testUser: User = {
-  username: 'test',
-  email: 'abc@xyz.com',
-  type: 'teacher'
-}
