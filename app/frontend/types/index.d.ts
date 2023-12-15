97ad3ac57aab5150d420313ab14eda1bd31f49d2
afkbeto <qv.nguyen00@gmail.com>
2023-12-15 17:48:25 +0100

    add barrel declaration file and move props to proper props file

---
+++ app/frontend/types/index.d.ts
@@ -0,0 +1,7 @@
+export * from './attachment';
+export * from './exam';
+export * from './language';
+export * from './module';
+export * from './props';
+export * from './question';
+export * from './user';
