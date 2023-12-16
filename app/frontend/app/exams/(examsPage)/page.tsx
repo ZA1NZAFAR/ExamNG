37cbf0801e2bf5ff75c2badcb4895b40bc01d7d2
afkbeto <qv.nguyen00@gmail.com>
2023-12-16 22:49:44 +0100

    update exam service using reworked types

--- app/frontend/app/exams/(examsPage)/page.tsx
+++ app/frontend/app/exams/(examsPage)/page.tsx
@@ -2,45 +2,14 @@
 
 import '@/styles/globals.css';
 import React, { useState, useEffect } from 'react';
-import { Exam } from '@/types/exam';
-import { ExamServiceProvider } from '../../contexts/providers/ExamServiceProvider';
-import { useExamService } from '../../hooks/useExamService';
+import { Exam } from '@/types';
 import { ExamCard } from '@/components/examCard';
-
-const ExamPageContent: React.FC = () => {
-	const [exams, setExams] = useState<Array<Exam>>([]);
-	const examService = useExamService();
-
-	// Fetch exams when the Exam page loads
-	useEffect(() => {
-		const fetchExams = async () => {
-			try {
-				const fetchedExams = await examService.getExams();
-				setExams(fetchedExams);
-			} catch (error) {
-				console.error('Error fetching exams:', error);
-			}
-		};
-
-		fetchExams();
-	}, [examService]);
-
-	return (
-		<div>
-			<div className='flex flex-row flex-wrap gap-4 lg:gap-8 place-content-center w-full'>
-				{ exams.map((exam) => (
-					<ExamCard key={ exam.id } exam={ exam } />
-				)) }
-			</div>
-		</div>
-	);
-};
+import { useService } from '@/hooks/useService';
 
 export default function ExamPage() {
 	// TODO: verify if we still need isSidebarCollapsed
 	/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 	const [isSidebarCollapsed, setIsSidebarCollapsed] = useState<boolean>(true);
-	const apiUrl = 'https://exam-ng.com';
 	/*
 	TODO: might be useful
 	const toggleSidebarCollapseHandler = () => {
@@ -48,9 +17,37 @@ export default function ExamPage() {
 	};
 	*/
 
+	const [exams, setExams] = useState<Exam[]>([]);
+	const { examService } = useService();
+
+	// Fetch exams when the Exam page loads
+	useEffect(() => {
+		(async () => {
+			try {
+				const fetchedModules = await examService.getModules();
+				const fetchedExams: Exam[] = [];
+				for (const fetchedModule of fetchedModules.results) {
+					const response = await examService.getExams(fetchedModule.code);
+					fetchedExams.push(...response.results);
+				}
+				
+				setExams(fetchedExams);
+			} catch (error) {
+				console.error('Error fetching exams:', error);
+			}
+		})();
+	}, [examService]);
+
 	return (
-		<ExamServiceProvider apiUrl={apiUrl}>
-			<ExamPageContent />
-		</ExamServiceProvider>
+		<div>
+			<div className='flex flex-row flex-wrap gap-4 lg:gap-8 place-content-center w-full'>
+				{ exams.map ((exam) => (
+					<ExamCard
+						key={ exam.id }
+						exam={ exam }
+					/>
+				)) }
+			</div>
+		</div>
 	);
 }
--- app/frontend/services/examService.ts
+++ app/frontend/services/examService.ts
@@ -1,22 +1,45 @@
 import { envConfig } from '@/config/envConfig';
-import { Exam } from '@/types/module';
+import { Exam, Module, Question, Result } from '@/types';
+import axios from 'axios';
 
 export class ExamService {
-	async getModules() {
-		return await fetch(`${envConfig.backendAPI}/modules`).then(res => res.json());
+  
+	async getModules(): Promise<Result<Module>> {
+		const response = await axios.get<Result<Module>>(`${envConfig.backendAPI}/modules`);
+		return response.data;
 	}
 
-	async getExams(moduleCode: string) {
-		return await fetch(`${envConfig.backendAPI}/modules/${moduleCode}`).then(res => res.json());
+	async getExams(moduleCode: string): Promise<Result<Exam>> {
+		const response = await axios.get<Result<Exam>>(`${envConfig.backendAPI}/modules/${moduleCode}`);
+		return response.data;
 	}
 
-	async getExam(moduleCode: string, examId: string): Promise<Exam> {
-		// eslint-disable-next-line no-useless-catch 
-		try {
-			const response = await fetch(`${envConfig.backendAPI}/modules/${moduleCode}/${examId}`);
-			return await response.json();
-		} catch (error) {
-			throw error;
-		}
+	async getExamById(moduleCode: string, examId: string): Promise<Exam> {
+		const response = await axios.get<Exam>(`${envConfig.backendAPI}/modules/${moduleCode}/${examId}`);
+		return response.data;
+	}
+
+	async updateExam(moduleCode: string, examId: string, exam: Exam): Promise<Exam> {
+		const response = await axios.put<Exam>(`${envConfig.backendAPI}/modules/${moduleCode}/${examId}`, exam);
+		return response.data;
+	}
+
+	async getExamQuestions(moduleCode: string, examId: string): Promise<Result<Question>> {
+		const response = await axios.get<Result<Question>>(`${envConfig.backendAPI}/modules/${moduleCode}/${examId}/questions`);
+		return response.data;
+	}
+
+	async createExamQuestion(moduleCode: string, examId: string, question: Question): Promise<Question> {
+		const response = await axios.post<Question>(`${envConfig.backendAPI}/modules/${moduleCode}/${examId}/questions`, question);
+		return response.data;
+	}
+
+	async updateExamQuestion(moduleCode: string, examId: string, questionId: string, question: Question): Promise<Question> {
+		const response = await axios.put<Question>(`${envConfig.backendAPI}/modules/${moduleCode}/${examId}/questions/${questionId}`, question);
+		return response.data;
+	}
+
+	async deleteExamQuestion(moduleCode: string, examId: string, questionId: string): Promise<void> {
+		await axios.delete(`${envConfig.backendAPI}/modules/${moduleCode}/${examId}/questions/${questionId}`);
 	}
 }
