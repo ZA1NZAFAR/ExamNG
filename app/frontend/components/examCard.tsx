a5a4c84b3f62b1c530f586cce84d7f8cac8b0eba
afkbeto <qv.nguyen00@gmail.com>
2023-12-17 17:50:57 +0100

    refactor code

--- app/frontend/components/examCard.tsx
+++ app/frontend/components/examCard.tsx
@@ -1,11 +1,81 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, MutableRefObject } from 'react';
 import Image from 'next/image';
 import { Card, CardBody, Progress, Switch } from '@nextui-org/react';
 import { Link } from '@nextui-org/link';
 import { Bell, BellOff, UsersRound, Book, Calendar, BarChart2 } from 'lucide-react';
 import { Exam  } from '@/types';
-import logo from '../resources/img/logo.png';
+import logo from '@/resources/img/logo.png';
 import { envConfig } from '@/config/envConfig';
+
+const dateDifferenceInDays = (date1: Date, date2: Date): number => {
+	const MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+	// Discard the time and time-zone information
+	const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+	const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+
+	return Math.floor((utc2 - utc1) / MS_PER_DAY);
+};
+
+const formatDateToString = (timestamp: number): string => {
+	const date = new Date(timestamp);
+	const currentDate = new Date();
+	const year = date.getFullYear();
+	const month = (date.getMonth() + 1) < 10 ? ('0' + (date.getMonth() + 1)) : (date.getMonth() + 1);		// Month is zero-based, so add 1
+	const day = date.getDate() < 10 ? ('0' + date.getDate()) : date.getDate();
+	const hour = date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours();
+	const minute = date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes();
+
+	let formattedDate = day + '/' + month + '/' + year + ' ' + hour + ':' + minute + ' ';
+	const daysDifference = dateDifferenceInDays(currentDate, date);
+	const daysFormatted = Math.abs(daysDifference) > 1 ? 'days' : 'day';
+
+	if (daysDifference < 0) {
+		formattedDate += `(${-daysDifference} ${daysFormatted} ago)`;
+	} else if (daysDifference > 0) {
+		formattedDate += `(in ${daysDifference} ${daysFormatted})`;
+	} else {
+		formattedDate += '(exam is today)';
+	}
+
+	return formattedDate;
+};
+
+const doExamProgressCalculation = (
+	startTimestamp: number,
+	endTimestamp: number,
+	intervalIDRef: MutableRefObject<NodeJS.Timeout | null>,
+	setExamProgress: ((a: number) => void) ) => {
+	const startTime = new Date(startTimestamp);
+	const endTime = new Date(endTimestamp);
+	const currentTime = new Date();
+	const MS_PER_MINUTE = 60_000;
+
+	// Exam is planned, hasn't yet started
+	if (currentTime < startTime) {
+		if (intervalIDRef.current) {
+			clearInterval(intervalIDRef.current);
+			intervalIDRef.current = null;
+		}
+
+		setExamProgress(0);
+		// Exam has taken place in the past
+	} else if (currentTime > endTime) {
+		if (intervalIDRef.current) {
+			clearInterval(intervalIDRef.current);
+			intervalIDRef.current = null;
+		}
+
+		setExamProgress(100);
+		// Exam is in progress
+	} else {
+		const diffMsDuration = (endTime.getTime() - startTime.getTime());					// milliseconds between end time & start time
+		const diffMsPastTimeSinceStart = (currentTime.getTime() - startTime.getTime());		// milliseconds between now & start time
+		const diffMinsDuration = Math.round(diffMsDuration / MS_PER_MINUTE);
+		const diffMinsPastTimeSinceStart = Math.round(diffMsPastTimeSinceStart / MS_PER_MINUTE);
+		setExamProgress(diffMinsPastTimeSinceStart / diffMinsDuration * 100);
+	}
+};
 
 /**
  * Represents the properties for an exam card.
@@ -24,78 +94,14 @@ export const ExamCard = ({ exam } : ExamCardProps) => {
 	const intervalIDRef = useRef<NodeJS.Timeout | null>(null);
 
 	const { module, groups, average } = exam.summaryFields;
+  
+	const calculateExamProgress = (startTimestamp: number, endTimestamp: number) => doExamProgressCalculation(startTimestamp, endTimestamp, intervalIDRef, setExamProgress); 
 
-	const formatDate = (timestamp: number): string => {
-		const date = new Date(timestamp);
-		const currentDate = new Date();
-		const year = date.getFullYear();
-		const month = (date.getMonth() + 1) < 10 ? ('0' + (date.getMonth() + 1)) : (date.getMonth() + 1);		// Month is zero-based, so add 1
-		const day = date.getDate() < 10 ? ('0' + date.getDate()) : date.getDate();
-		const hour = date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours();
-		const minute = date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes();
-
-		let formattedDate = day + '/' + month + '/' + year + ' ' + hour + ':' + minute + ' ';
-		const daysDifference = dateDifferenceInDays(currentDate, date);
-		const daysFormatted = Math.abs(daysDifference) > 1 ? 'days' : 'day';
-
-		if (daysDifference < 0) {
-			formattedDate += `(${-daysDifference} ${daysFormatted} ago)`;
-		} else if (daysDifference > 0) {
-			formattedDate += `(in ${daysDifference} ${daysFormatted})`;
-		} else {
-			formattedDate += '(exam is today)';
-		}
-
-		return formattedDate;
-	};
-
-	const dateDifferenceInDays = (date1: Date, date2: Date): number => {
-		const MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-		// Discard the time and time-zone information
-		const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
-		const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
-	
-		return Math.floor((utc2 - utc1) / MS_PER_DAY);
-	};
-
-	const classAvgGrade = (): string => {
+	const classAverageToString = (): string => {
 		if (!average) {
 			return 'N/A';
 		}
 		return average + '/' + envConfig.defaultTotalScore;
-	};
-
-	const calculateExamProgress = (startTimestamp: number, endTimestamp: number) => {
-		const startTime = new Date(startTimestamp);
-		const endTime = new Date(endTimestamp);
-		const currentTime = new Date();
-		const MS_PER_MINUTE = 60_000;
-
-		// Exam is planned, hasn't yet started
-		if (currentTime < startTime) {
-			if (intervalIDRef.current) {
-				clearInterval(intervalIDRef.current);
-				intervalIDRef.current = null;
-			}
-
-			setExamProgress(0);
-		// Exam has taken place in the past
-		} else if (currentTime > endTime) {
-			if (intervalIDRef.current) {
-				clearInterval(intervalIDRef.current);
-				intervalIDRef.current = null;
-			}
-
-			setExamProgress(100);
-		// Exam is in progress
-		} else {
-			const diffMsDuration = (endTime.getTime() - startTime.getTime());					// milliseconds between end time & start time
-			const diffMsPastTimeSinceStart = (currentTime.getTime() - startTime.getTime());		// milliseconds between now & start time
-			const diffMinsDuration = Math.round(diffMsDuration / MS_PER_MINUTE);
-			const diffMinsPastTimeSinceStart = Math.round(diffMsPastTimeSinceStart / MS_PER_MINUTE);
-			setExamProgress(diffMinsPastTimeSinceStart / diffMinsDuration * 100);
-		}
 	};
 
 	useEffect(() => {
@@ -160,13 +166,13 @@ export const ExamCard = ({ exam } : ExamCardProps) => {
 							<div className='flex flex-row justify-start items-center text-xs xl:text-small text-foreground/80'>
 								<Calendar className='mr-2' />
 								<div className='w-14 h-14 block overflow-hidden text-start text-ellipsis white-space:nowrap hover:overflow-visible xl:w-24 xl:h-auto'>
-									<p>{ formatDate(exam.startTimestamp) }</p>
+									<p>{ formatDateToString(exam.startTimestamp) }</p>
 								</div>
 							</div>
 							<div className='flex flex-row justify-start items-center text-small text-foreground/80'>
 								<BarChart2 className='mr-2' />
 								<div className='w-14 h-14 block overflow-hidden text-end text-ellipsis white-space:nowrap hover:overflow-visible xl:w-24 xl:h-auto'>
-									<p className='inline-block'>{ classAvgGrade() }</p>
+									<p className='inline-block'>{ classAverageToString() }</p>
 								</div>
 							</div>
 						</div>
@@ -188,8 +194,8 @@ export const ExamCard = ({ exam } : ExamCardProps) => {
 							showValueLabel={true}
 						></Progress>
 						<div className="flex justify-between">
-							<p className="text-small">{ formatDate(exam.startTimestamp).split(' ')[1] }</p>
-							<p className={`text-small ${examProgress > 0 && examProgress < 100 ? 'text-foreground/50' : ''}`}>{ formatDate(exam.endTimestamp).split(' ')[1] }</p>
+							<p className="text-small">{ formatDateToString(exam.startTimestamp).split(' ')[1] }</p>
+							<p className={`text-small ${examProgress > 0 && examProgress < 100 ? 'text-foreground/50' : ''}`}>{ formatDateToString(exam.endTimestamp).split(' ')[1] }</p>
 						</div>
 					</div>
 				</div>
