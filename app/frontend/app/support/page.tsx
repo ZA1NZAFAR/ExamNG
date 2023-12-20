6d06c52e3093db369ecc26d3ec6de77808bcfc38
Rimberse <kerimpltm@gmail.com>
2023-12-20 23:36:49 +0100

    Create Support component, embed it into support page

--- app/frontend/app/support/page.tsx
+++ app/frontend/app/support/page.tsx
@@ -1,9 +1,11 @@
 import { title } from '@/components/primitives';
+import { Support } from '@/components/support';
 
 export default function SupportPage() {
 	return (
 		<div>
 			<h1 className={title()}>Support</h1>
+			<Support />
 		</div>
 	);
 }
---
+++ app/frontend/components/support.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { envConfig } from '@/config/envConfig';
+
+export const Support = () => {
+	return (
+		<div>
+            Support
+		</div>
+	);
+};
