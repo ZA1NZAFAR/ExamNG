83334ca6d4c47fd5a0e6499a1cf2374ba3b8d508
Rimberse <kerimpltm@gmail.com>
2023-12-21 23:43:39 +0100

    Remove placeholder, add Card element

--- app/frontend/app/support/page.tsx
+++ app/frontend/app/support/page.tsx
@@ -1,11 +1,7 @@
-import { title } from '@/components/primitives';
 import { Support } from '@/components/support';
 
 export default function SupportPage() {
 	return (
-		<div>
-			<h1 className={title()}>Support</h1>
-			<Support />
-		</div>
+		<Support />
 	);
 }
--- app/frontend/components/support.tsx
+++ app/frontend/components/support.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Card, CardBody } from '@nextui-org/react';
 // import { envConfig } from '@/config/envConfig';
 
 export const Support = () => {
 	return (
-		<div>
-            Support
-		</div>
+		<Card className='flex flex-row justify-center items-center text-center w-3/4 h-3/4'>
+			<CardBody>
+				<p>Support</p>
+			</CardBody>
+		</Card>
 	);
 };
