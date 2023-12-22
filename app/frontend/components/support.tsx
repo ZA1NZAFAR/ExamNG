1a1158164b515bc3cf212e50abadf14344372d9c
Rimberse <kerimpltm@gmail.com>
2023-12-22 02:14:26 +0100

    Style subheading on support section

--- app/frontend/components/support.tsx
+++ app/frontend/components/support.tsx
@@ -6,8 +6,8 @@ export const Support = () => {
 	return (
 		<Card className='w-1/2 h-1/2 mt-12'>
 			<CardBody className='flex flex-col justify-center items-start p-14'>
-				<h3 className='text-5xl font-semibold tracking-wide antialiased mb-12'>Stuck in a pickle?</h3>
-				<p>We&apos;ve got the answer!<br/>Don&apos;t hesitate to throw us a line for a rescue mission 🚀</p>
+				<h3 className='text-5xl font-semibold tracking-wide antialiased mb-10 text-gray-800'>Stuck in a pickle?</h3>
+				<p><span className='text-left font-normal tracking-normal text-lg text-gray-700/50'>We&apos;ve got the answer!<br/>Don&apos;t hesitate to throw us a line for a rescue mission</span> 🚀</p>
 			</CardBody>
 		</Card>
 	);
