4fab2ccb85c387a12431df5d2770322859dcabc2
afkbeto <qv.nguyen00@gmail.com>
2023-12-28 18:25:58 +0100

    allow html conversion in statement input
    and render the html

--- app/frontend/components/question/question.tsx
+++ app/frontend/components/question/question.tsx
@@ -5,6 +5,7 @@ import { Divider } from '@nextui-org/divider';
 import { ExamContext } from './examContext';
 import AttachmentComponent from './attachment/attachment';
 import AnswerComponent from './answer/answer';
+import SafeHTML from '../safeHtml';
 
 
 /**
@@ -46,7 +47,7 @@ const QuestionComponent: React.FC<QuestionProps> = ({
 		<Card className="w-full text-start">
 			<CardHeader className="flex gap-3">
 				<div className="flex flex-col">
-					<p className="text-md">{id}. {question.statement}</p>
+					<div className="text-md">{id}. <SafeHTML html={question.statement} /></div>
 					<p className={showScore}>Score: {question.coefficient}/{examContext.totalScore}</p>
 				</div>
 			</CardHeader>
--- app/frontend/components/questionForm/statementInput.tsx
+++ app/frontend/components/questionForm/statementInput.tsx
@@ -1,22 +1,28 @@
 import { Editor as TextEditor } from 'react-draft-wysiwyg';
-import { EditorState, ContentState } from 'draft-js';
+import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import '@/node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { QuestionFormContext } from './questionFormContext';
 import React from 'react';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+import HTMLSanitize from '@/utils/htmlSanitizer';
 
 const StatementInput = () => {
+	const [ editorState, setEditorState ] = React.useState(EditorState.createEmpty());
 	const { question, setQuestion, errors, setErrors, deleteError } = React.useContext(QuestionFormContext);
-	const [ editorState, setEditorState ] = React.useState(
-		question.statement.length > 0 ?  EditorState.createWithContent(ContentState.createFromText(question.statement)) :
-			EditorState.createEmpty()
-	);
+	React.useEffect(() => {
+		const contentBlock = htmlToDraft(question.statement);
+		const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+		setEditorState(EditorState.createWithContent(contentState));
+	}, []);
 
 	const onEditorStateChange = (editorState: EditorState) => {
 		setEditorState(editorState);
-		const contentState = editorState.getCurrentContent().getPlainText();
-		setQuestion({ ...question, statement: contentState });
+		const newContentState = editorState.getCurrentContent();
+		const newStatement = HTMLSanitize(draftToHtml(convertToRaw(newContentState)));
+		setQuestion({ ...question, statement: newStatement });
 		const newErrors = {...errors};
-		if (contentState.length === 0) {
+		if (newContentState.getPlainText() === '') {
 			setErrors({...newErrors, statement: 'Statement cannot be empty'});
 		} else {
 			deleteError('statement');
@@ -30,9 +36,22 @@ const StatementInput = () => {
 				editorClassName="border-2 border-gray-300 rounded-md px-2 min-h-[100px]"
 				editorState={editorState}
 				onEditorStateChange={onEditorStateChange}
+				toolbar={{
+					options: ['inline', 'list', 'textAlign', 'remove', 'history'],
+					inline: {
+						options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace', 'superscript', 'subscript'],
+					},
+					list: {
+						options: ['unordered', 'ordered'],
+					},
+					textAlign: {
+						options: ['left', 'center', 'right', 'justify'],
+					}
+				}}
 			/>
 			{errors.statement && <p className="text-red-500">{errors.statement}</p>}
 		</>
+    
 	);
 };
 
