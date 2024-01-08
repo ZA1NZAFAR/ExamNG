60abd4d431196ec6bdc9b69726921eb10471156d
afkbeto <qv.nguyen00@gmail.com>
2024-01-08 23:05:39 +0100

    refactor answerForm to reduce useless rerendering

--- app/frontend/components/questionForm/answerForm.tsx
+++ app/frontend/components/questionForm/answerForm.tsx
@@ -6,55 +6,45 @@ import MCOptionInput from './MCOptionInput';
 import CodeEditor from '../codeEditor';
 import { CodeQuestion, MCQuestion } from '@/types';
 
-const AnswerForm: React.FC = () => {
+const MCQOptionList: React.FC = () => {
 	const [ render, setRender ] = React.useState<boolean>(false);
 	const {
-		type,
-		defaultLanguage,
-		initialCode,
-		setCodeQuestionField,
 		options,
 		addNewOption,
 		optionsErrors,
 		setOptionsError,
 		deleteOptionsError
-	} = useQuestionFormStore(useShallow((state) => ({
-		type: state.question.type,
-		defaultLanguage: (state.question as CodeQuestion).defaultLanguage,
-		initialCode: (state.question as CodeQuestion).initialCode,
-		setCodeQuestionField: (key: keyof CodeQuestion, value: string = '') => {
-			state.setQuestion({
-				...state.question,
-				[key]: value
-			} as CodeQuestion);
-		},
-		options: (state.question as MCQuestion).options,
-		addNewOption: () => {
-			const currentMCQuestion = state.question as MCQuestion;
-			state.setQuestion({
-				...state.question,
-				options: [...currentMCQuestion.options, {
-					statement: '',
-					isCorrectOption: false,
-				}]
-			} as MCQuestion);
-			state.setErrors({
-				...state.errors,
-				[`option${currentMCQuestion.options.length}-statement`]: 'Option statement cannot be empty'
-			});
-		},
-		optionsErrors: Object.keys(state.errors)
-			.filter((key) => key.startsWith('options-'))
-			.reduce((obj, key) => {
-				obj[key] = state.errors[key];
-				return obj;
-			}, {} as Record<string, string>),
-		setOptionsError: (errorType: string, newOptionError: string) => state.setErrors({
-			...state.errors,
-			[`options-${errorType}`]: newOptionError
-		}),
-		deleteOptionsError: (errorType: string) => state.deleteError(`options-${errorType}`)
-	})));
+	} = useQuestionFormStore(useShallow((state) => {
+		const currentMCQOptions = (state.question as MCQuestion).options;
+		const currentErrors = state.errors;
+		return {
+			options: currentMCQOptions,
+			addNewOption: () => {
+				state.setQuestion({
+					...state.question,
+					options: [...currentMCQOptions, {
+						statement: '',
+						isCorrectOption: false,
+					}]
+				} as MCQuestion);
+				state.setErrors({
+					...currentErrors,
+					[`option${currentMCQOptions.length}-statement`]: 'Option statement cannot be empty'
+				});
+			},
+			optionsErrors: Object.keys(currentErrors)
+				.filter((key) => key.startsWith('options-'))
+				.reduce((obj, key) => {
+					obj[key] = currentErrors[key];
+					return obj;
+				}, {} as Record<string, string>),
+			setOptionsError: (errorType: string, newOptionError: string) => state.setErrors({
+				...currentErrors,
+				[`options-${errorType}`]: newOptionError
+			}),
+			deleteOptionsError: (errorType: string) => state.deleteError(`options-${errorType}`)
+		};
+	}));
 
 	const optionsQuantityValidator = () => {
 		const hasAtLeastTwoOptions = () => options.length >= 2;
@@ -97,39 +87,69 @@ const AnswerForm: React.FC = () => {
 	}
 
 	React.useEffect(() => {
-		if (type === 'mcq') {
-			validateOptions();
-		}
+		validateOptions();
 		// F**k you React
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [render]);
+	return (
+		<div className="p-4">
+			<div className="flex flex-col gap-4 py-4">
+				{ options.map((_, index) => (
+					<MCOptionInput key={index} index={index} onInputChange={reloadComponent} />
+				))}
+				{ Object.keys(optionsErrors).map((errorType, index) => (
+					<span className="text-red-500" key={index}>{optionsErrors[errorType]}</span>
+				))}
+				<Button color="secondary" onPress={addNewOption}>Add new choice</Button>
+			</div>
+		</div>
+	);
+};
+
+const CodeSampleAnswer: React.FC = () => {
+	const {
+		defaultLanguage,
+		initialCode,
+		setCodeQuestionField,
+	} = useQuestionFormStore(useShallow((state) => ({
+		type: state.question.type,
+		defaultLanguage: (state.question as CodeQuestion).defaultLanguage,
+		initialCode: (state.question as CodeQuestion).initialCode,
+		setCodeQuestionField: (key: keyof CodeQuestion, value: string = '') => {
+			state.setQuestion({
+				...state.question,
+				[key]: value
+			} as CodeQuestion);
+		},
+	})));
+	return (
+		<div className="flex flex-col border-2 border-gray-300 rounded-md py-2 px-4 gap-4">
+			<span className="text-gray-500">Add an initial code for the answer.</span>
+			<CodeEditor
+				language={defaultLanguage}
+				code={initialCode}
+				onCodeChange={(code) => setCodeQuestionField('initialCode', code)}
+				onLanguageChange={(language) => setCodeQuestionField('defaultLanguage', language)}
+			/>
+		</div>
+	);
+};
+
+const AnswerForm: React.FC = () => {
+	const {
+		type,
+	} = useQuestionFormStore(useShallow((state) => ({
+		type: state.question.type,
+	})));
 
 	if (type === 'mcq') {
 		return (
-			<div className="p-4">
-				<div className="flex flex-col gap-4 py-4">
-					{ options.map((_, index) => (
-						<MCOptionInput key={index} index={index} onInputChange={reloadComponent} />
-					))}
-					{ Object.keys(optionsErrors).map((errorType, index) => (
-						<span className="text-red-500" key={index}>{optionsErrors[errorType]}</span>
-					))}
-					<Button color="secondary" onPress={addNewOption}>Add new choice</Button>
-				</div>
-			</div>
+			<MCQOptionList />
 		);
 	}
 	if (type === 'code') {
 		return (
-			<div className="flex flex-col border-2 border-gray-300 rounded-md py-2 px-4 gap-4">
-				<span className="text-gray-500">Add an initial code for the answer.</span>
-				<CodeEditor
-					language={defaultLanguage}
-					code={initialCode}
-					onCodeChange={(code) => setCodeQuestionField('initialCode', code)}
-					onLanguageChange={(language) => setCodeQuestionField('defaultLanguage', language)}
-				/>
-			</div>
+			<CodeSampleAnswer />
 		);
 	}
 	return (
