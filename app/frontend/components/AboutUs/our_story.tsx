aa97b297029e72534afc8a167f533cb5ad18fb47
afkbeto <qv.nguyen00@gmail.com>
2024-01-12 12:39:33 +0100

    cleanup

--- app/frontend/components/AboutUs/our_story.tsx
+++ app/frontend/components/AboutUs/our_story.tsx
@@ -10,17 +10,11 @@ export default function Our_story() {
 			</div>
 			<p className={style.TextTitle}>Our story</p>
 			<p className={`${style.story} mt-20`}>
-				{ 'Once upon a time, a group of old school friends embarked on a journey. It all started with a year-end project that sparked a shared passion. Today, that passion drives us as we lead you through a transformative technological adventure in the realm of exams.' }
-			</p>
-			<p className={style.story}>
-				{ 'With every step, we\'re committed to weaving a narrative of innovation and improvement.' }
+				{ 'Once upon a time, a group of old school friends embarked on a journey. It all started with a year-end project that sparked a shared passion. Today, that passion drives us as we lead you through a transformative technological adventure in the realm of exams. With every step, we\'re committed to weaving a narrative of innovation and improvement.' }
 			</p>
 
 			<p className={style.story}>
-				{ 'Our constant quest? To bring students, teachers, and proctors closer together, shaping an experience often deemed \'arduous\' into one that\'s beautifully intuitive.' }
-			</p>
-			<p className={style.story}>
-                And that, dear reader, concludes this part about our tale of friendship, innovation, and transforming the exam experience!
+				{ 'Our constant quest? To bring students, teachers, and proctors closer together, shaping an experience often deemed \'arduous\' into one that\'s beautifully intuitive. And that, dear reader, concludes this part about our tale of friendship, innovation, and transforming the exam experience!' }
 			</p>
 
 		</div>
