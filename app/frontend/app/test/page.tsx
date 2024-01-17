c8a62697093725255a5bb126aba00359ce9d9695
mrykse <meryem.kose@hotmail.fr>
2024-01-17 16:39:50 +0100

    add forbidden copy and paste

--- app/frontend/app/test/page.tsx
+++ app/frontend/app/test/page.tsx
@@ -1,10 +1,17 @@
+'use client';
+import React from 'react';
 import FullScreenComponent from '@/components/Test/test';
 
 export default function CalendarPage() {
+	const handleCopyPaste = (event: { preventDefault: () => void; }) => {
+		event.preventDefault();
+		alert('Copying and pasting is not allowed on this page.');
+	};
+
 	return (
-		<div>
+		<div onCopy={handleCopyPaste} onPaste={handleCopyPaste}>
 			<h1>Test Page</h1>
-			<FullScreenComponent/>
+			<FullScreenComponent />
 		</div>
 	);
 }
