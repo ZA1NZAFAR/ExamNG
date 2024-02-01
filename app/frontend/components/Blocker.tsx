4c017f8e8bae7fbff0906c808352d437fe536994
afkbeto <qv.nguyen00@gmail.com>
2024-02-01 22:17:33 +0100

    fix: prevent F11 from breaking Blocker

--- app/frontend/components/Blocker.tsx
+++ app/frontend/components/Blocker.tsx
@@ -6,15 +6,17 @@ import { Button } from '@nextui-org/react';
 import React from 'react';
 
 const Blocker = () => {
+	const bgColors = ['bg-zinc-700', 'bg-red-700', 'bg-blue-700', 'bg-yellow-700', 'bg-orange-700', 'bg-green-700', 'bg-pink-700', 'bg-purple-700', 'bg-indigo-700', 'bg-gray-700'];
 	const [render, setRender] = React.useState(false);
+	const [ bgString, setBgString ] = React.useState('bg-zinc-700');
 
 	const isFullscreen = React.useMemo(() => {
 		return !window.screenTop && !window.screenY;
 	}, [window.screenTop, window.screenY, render]);
 	
 	console.log(isFullscreen);
-	const className = !isFullscreen ? 'fixed inset-x-0 top-0 z-30 flex items-center justify-center w-full h-full bg-zinc-600 opacity-90' : 'hidden';
-
+	const className = !isFullscreen ? `fixed inset-x-0 top-0 z-30 flex items-center justify-center w-full h-full opacity-90 ${bgString}` : 'hidden';
+	
 	const handleRequestFullscreen = async () => {
 		try {
 			window.addEventListener('fullscreenchange', (_) => {
@@ -26,6 +28,28 @@ const Blocker = () => {
 			console.log(error);
 		}
 	};
+
+	React.useEffect(() => {
+		const disableF11 = (e: KeyboardEvent) => {
+			if (e.key === 'F11') {
+				e.preventDefault();
+			}
+		};
+		window.addEventListener('keydown', disableF11);
+		const interval = setInterval(() => {
+			if (!isFullscreen) {
+				let index = bgColors.indexOf(bgString);
+				while (bgColors[index] === bgString || index === -1) {
+					index = Math.floor(Math.random() * bgColors.length);
+				}
+				console.log(bgColors[index]);
+				setBgString(bgColors[index]);
+			}
+		}, 1000);
+		return () => {
+			clearInterval(interval);
+		}
+	},[isFullscreen, bgColors, bgString]);
 	return (
 		<div
 			className={className}
