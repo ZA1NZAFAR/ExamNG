9ea3108262c95aaec3f2aab96b06f56bccc5d925
afkbeto <qv.nguyen00@gmail.com>
2024-02-01 22:28:59 +0100

    some cleanups

--- app/frontend/components/Blocker.tsx
+++ app/frontend/components/Blocker.tsx
@@ -1,26 +1,29 @@
 
 'use client';
 
-import { useService } from '@/hooks/useService';
 import { Button } from '@nextui-org/react';
 import React from 'react';
 
 const Blocker = () => {
 	const bgColors = ['bg-zinc-700', 'bg-red-700', 'bg-blue-700', 'bg-yellow-700', 'bg-orange-700', 'bg-green-700', 'bg-pink-700', 'bg-purple-700', 'bg-indigo-700', 'bg-gray-700'];
-	const [render, setRender] = React.useState(false);
 	const [ bgString, setBgString ] = React.useState('bg-zinc-700');
 
-	const isFullscreen = React.useMemo(() => {
-		return !window.screenTop && !window.screenY;
-	}, [window.screenTop, window.screenY, render]);
+	const isFullscreen = document.fullscreenElement !== null;
 	
-	console.log(isFullscreen);
 	const className = !isFullscreen ? `fixed inset-x-0 top-0 z-30 flex items-center justify-center w-full h-full opacity-90 ${bgString}` : 'hidden';
+
+	const getRandomColor = () => {
+		let index = bgColors.indexOf(bgString);
+		while (bgColors[index] === bgString || index === -1) {
+			index = Math.floor(Math.random() * bgColors.length);
+		}
+		return bgColors[index];
+	}
 	
 	const handleRequestFullscreen = async () => {
 		try {
 			window.addEventListener('fullscreenchange', (_) => {
-				setRender(!!document.fullscreenElement);
+				setBgString(getRandomColor());
 				window.localStorage.setItem('fullscreen', document.fullscreenElement ? 'true' : 'false');
 			});
 			await document.documentElement.requestFullscreen();
@@ -38,18 +41,13 @@ const Blocker = () => {
 		window.addEventListener('keydown', disableF11);
 		const interval = setInterval(() => {
 			if (!isFullscreen) {
-				let index = bgColors.indexOf(bgString);
-				while (bgColors[index] === bgString || index === -1) {
-					index = Math.floor(Math.random() * bgColors.length);
-				}
-				console.log(bgColors[index]);
-				setBgString(bgColors[index]);
+				setBgString(getRandomColor());
 			}
 		}, 1000);
 		return () => {
 			clearInterval(interval);
 		}
-	},[isFullscreen, bgColors, bgString]);
+	},[isFullscreen, bgString]);
 	return (
 		<div
 			className={className}
